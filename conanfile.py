9b37af8031998c8c70ef6389e27fc74764804472
bincrafters-user <bincrafters@gmail.com>
2020-07-20 05:32:59 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.5

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class SDL2TtfConan(ConanFile):
         "fPIC": True,
     }
     requires = (
-        "freetype/2.10.1",
+        "freetype/2.10.2",
         "sdl2/2.0.9@bincrafters/stable",
     )
     _source_subfolder = "source_subfolder"
